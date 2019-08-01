1bad649ea98848ce088e454cfe724a9cd8f2e232
Alan Hernandez <this.alan856@gmail.com>
2019-07-31 23:42:22 -0500

    config for application

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+
+class Config:
+	SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
+
+class DevelopmentConfig(Config):
+	DEBUG = True
+	SQLALCHEMY_DATABASE_URI = 'mysql://root:lol123@localhost/api_flask'
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class TestingConfig(Config):
+	SQLALCHEMY_DATABASE_URI = 'mysql://root:lol123@localhost/api_flask_test'
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	TEST = True
+	DEBUG = True
+
+config = {
+	'development': DevelopmentConfig,
+	'default': DevelopmentConfig,
+	'test': TestingConfig
+}
