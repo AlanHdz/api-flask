ac054c39d3dc8c8b09d60c77d9c6c2cc9104537f
Alan Hernandez <this.alan856@gmail.com>
2019-07-31 23:53:18 -0500

    little changes in url database

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import os
 
 class Config:
-	SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
+	SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
 
 class DevelopmentConfig(Config):
 	DEBUG = True
-	SQLALCHEMY_DATABASE_URI = 'mysql://root:lol123@localhost/api_flask'
+	SQLALCHEMY_DATABASE_URI = 'mysql://username:password@host/db_name'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class TestingConfig(Config):
-	SQLALCHEMY_DATABASE_URI = 'mysql://root:lol123@localhost/api_flask_test'
+	SQLALCHEMY_DATABASE_URI = 'mysql://username:password@host/db_name'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	TEST = True
 	DEBUG = True
