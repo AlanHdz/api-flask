e485d1c6173aea1283290b7226f65807084153e9
Alan Hernandez <this.alan856@gmail.com>
2019-07-31 23:43:00 -0500

    manage for the application

---
+++ manage.py
@@ -0,0 +1,28 @@
+from app import create_app
+from app import db, User
+
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
+
+from config import config
+
+config_class = config['development']
+app = create_app(config_class)
+migrate = Migrate(app, db)
+
+def make_shell_context():
+	return dict(app=app, db=db, User=User)
+
+if __name__ == '__main__':
+	manager = Manager(app)
+
+	manager.add_command('Shell', Shell(make_context=make_shell_context))
+	manager.add_command('db', MigrateCommand)
+
+	@manager.command
+	def test():
+		import unittest
+		tests = unittest.TestLoader().discover('tests')
+		unittest.TextTestRunner().run(tests)
+
+	manager.run()
